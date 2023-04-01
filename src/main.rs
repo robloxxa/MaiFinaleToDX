2f916a62e5c7dfd45a7991f8322937d49dea8312
robloxxa <robloxxa@yandex.ru>
2023-04-01 14:33:35 +0300

    added logger

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use std::io::{BufReader, Read};
-use std::rc::Rc;
-use std::sync::Arc;
-use std::thread;
-use std::thread::JoinHandle;
-use std::time::{Duration, Instant};
-
 use ::clap;
-use ::crossbeam_channel;
-use ::serialport;
 use clap::Parser;
+use flexi_logger::{colored_opt_format, opt_format, Logger};
+use log::{debug, info, warn};
+use std::time::Instant;
 
+mod config;
 mod helper_funcs;
 mod jvs;
 mod touch;
@@ -18,44 +13,52 @@ mod touch;
 #[command(author = "robloxxa", version, about, long_about = None)]
 /// Tool that allow playing Maimai DX on original Maimai Finale Cabinet
 pub struct Arguments {
+    /// Log level, options: INFO, WARN,
+    #[arg(long, default_value = "info")]
+    pub log_level: String,
+
     /// When set to True (or presented) will disable touch features
     #[arg(long, default_value = "false")]
-    touch_disabled: bool,
+    pub disable_touch: bool,
 
     /// When set to True (or presented) will disable JVS features
     #[arg(long, default_value = "false")]
-    jvs_disabled: bool,
-
-    /// When set to True (or presented) will disable Proxying
-    #[arg(long, default_value = "false")]
-    reader_proxy_disabled: bool,
+    pub disable_jvs: bool,
 
     /// COM Port for Finale touch
     #[arg(long, default_value = "COM9")]
-    re2_touch_com: String,
+    pub touch_re2_com: String,
 
     /// COM Port for Deluxe Player 1 touch
     #[arg(long, default_value = "COM6")]
-    alls_p1_touch_com: String,
+    pub touch_alls_p1_com: String,
 
     /// COM Port for Deluxe Player 2 touch
     #[arg(long, default_value = "COM7")]
-    alls_p2_touch_com: String,
+    pub touch_alls_p2_com: String,
 
     /// COM Port for Finale's JVS
     #[arg(long, default_value = "COM24")]
-    re2_jvs_com: String,
+    pub jvs_re2_com: String,
 }
 
 fn main() {
     let args = Arguments::parse();
-    if !args.touch_disabled {
+    Logger::try_with_str(args.log_level.clone())
+        .unwrap()
+        .format(colored_opt_format)
+        .start()
+        .unwrap();
+    if !args.disable_touch {
         let (re2, alls) = touch::spawn_thread(&args);
         re2.join().unwrap();
         alls.join().unwrap();
+    } else {
+        warn!("\"disable_touch\" was set to True. Touch features disabled")
     }
-}
 
-fn spawn_jvs_thread() {
-    todo!()
+    if !args.disable_jvs {
+    } else {
+        warn!("\"disable_jvs\" was set to True. JVS features disabled")
+    }
 }
