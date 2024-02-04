cc157658ba24502a3274be5bfd590ae2f07bb7ce
robloxxa <robloxxa@yandex.ru>
2024-02-04 22:57:07 +0300

    splitting big main function in smaller *setup* functions for better reading and better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::config::Config;
-use anyhow::{Error, Result};
+use anyhow::{Context, Error, Result};
 use clap::Parser;
 use clap_serde_derive::ClapSerde;
 use config::Settings;
@@ -7,66 +7,72 @@ use flexi_logger::{colored_opt_format, Logger};
 use log::{error, info, warn};
 
 use std::fs::File;
-use std::io;
 use std::io::{Read, Write};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread::JoinHandle;
+
 use winapi::um::timeapi;
 
-mod card_reader;
+// mod card_reader;
 mod config;
 mod helper_funcs;
-mod jvs;
-mod keyboard;
-mod packets;
+// mod jvs;
+// mod keyboard;
+// mod packets;
 mod touch;
 
 fn main() {
-    let mut handles: Vec<JoinHandle<io::Result<()>>> = Vec::new();
+    // let mut handles: Vec<JoinHandle<io::Result<()>>> = Vec::new();
 
-    let running = Arc::new(AtomicBool::new(true));
-    if !args.settings.disable_touch {
-        match touch::spawn_thread(&args.settings, &running) {
-            Ok((finale, deluxe)) => {
-                handles.push(finale);
-                handles.push(deluxe);
-            }
-            Err(err) => error!("Touchscreen initialization failed: {}", err),
-        };
-    } else {
-        warn!("\"disable_touch\" was set to True. Touch features disabled")
+    // let running = Arc::new(AtomicBool::new(true));
+    // if !args.settings.disable_touch {
+    //     match touch::spawn_thread(&args.settings, &running) {
+    //         Ok((finale, deluxe)) => {
+    //             handles.push(finale);
+    //             handles.push(deluxe);
+    //         }
+    //         Err(err) => error!("Touchscreen initialization failed: {}", err),
+    //     };
+    // } else {
+    //     warn!("\"disable_touch\" was set to True. Touch features disabled")
+    // }
+
+    // if !args.settings.disable_jvs {
+    //     match jvs::spawn_thread(&args, running.clone()) {
+    //         Ok(jvs) => handles.push(jvs),
+    //         Err(err) => error!("JVS initialization failed: {}", err),
+    //     }
+    // } else {
+    //     warn!("\"disable_jvs\" was set to True. JVS features disabled")
+    // }
+
+    // if !args.settings.disable_reader {
+    //     match card_reader::spawn_thread(&args, running.clone()) {
+    //         Ok(reader) => handles.push(reader),
+    //         Err(err) => error!("Card reader initialization failed: {}", err),
+    //     }
+    // } else {
+    //     warn!("\"disable_reader\" was set to True. NFC reader proxy disabled")
+    // }
+
+    // ctrlc::set_handler(move || {
+    //     info!("Exiting...");
+    //     running.store(false, Ordering::Release);
+    // })
+    // .unwrap();
+
+    // for _ in 0..handles.len() {
+    //     if let Err(e) = handles.pop().unwrap().join() {
+    //         error!("Thread panicked, {:?}", e);
+    //     }
+    // }
+    if let Err(e) = setup() {
+        error!("Error: {}", e);
     }
 
-    if !args.settings.disable_jvs {
-        match jvs::spawn_thread(&args, running.clone()) {
-            Ok(jvs) => handles.push(jvs),
-            Err(err) => error!("JVS initialization failed: {}", err),
-        }
-    } else {
-        warn!("\"disable_jvs\" was set to True. JVS features disabled")
-    }
-
-    if !args.settings.disable_reader {
-        match card_reader::spawn_thread(&args, running.clone()) {
-            Ok(reader) => handles.push(reader),
-            Err(err) => error!("Card reader initialization failed: {}", err),
-        }
-    } else {
-        warn!("\"disable_reader\" was set to True. NFC reader proxy disabled")
-    }
-
-    ctrlc::set_handler(move || {
-        info!("Exiting...");
-        running.store(false, Ordering::Release);
-    })
-    .unwrap();
-
-    for _ in 0..handles.len() {
-        if let Err(e) = handles.pop().unwrap().join() {
-            error!("Thread panicked, {:?}", e);
-        }
-    }
+    use std::process::Command;
+	let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();
 }
 
 fn setup() -> Result<()> {
@@ -74,23 +80,32 @@ fn setup() -> Result<()> {
         timeapi::timeBeginPeriod(1);
     }
 
-
     let logger = Logger::try_with_str("info")?
         .format(colored_opt_format)
         .start()?;
 
     let config = setup_config()?;
-
     if config.log_level != "info" {
         logger.parse_new_spec(&config.log_level)?;
     }
 
+    let exit_sig = Arc::new(AtomicBool::new(false));
+
+    let handlers = setup_handlers(&config.settings, &exit_sig)?;
+
+    ctrlc::set_handler(move || {
+        info!("Got CTRL+C, exiting...");
+        exit_sig.store(true, Ordering::Relaxed);
+    })?;
+
+    for handler in handlers.into_iter() {
+        handler.join().unwrap().unwrap();
+    }
     Ok(())
 }
 
-
 fn setup_config() -> Result<Config> {
-    let mut config = Config::parse();
+    let config = Config::parse();
 
     if config.create_config {
         let config_str = toml::to_string_pretty(&config)?;
@@ -114,10 +129,18 @@ fn setup_config() -> Result<Config> {
     }
 }
 
-fn setup_handlers(cfg: &Config) -> Result<()> {
-    let (send, recv) = crossbeam_channel::bounded::<Error>(1);
+fn setup_handlers(
+    cfg: &Settings,
+    exit_sig: &Arc<AtomicBool>,
+) -> Result<Vec<JoinHandle<Result<()>>>> {
+    let mut handlers = vec![];
 
+    if cfg.touch {
+        match touch::setup(cfg, &exit_sig) {
+            Ok(v) => handlers.extend(v),
+            Err(e) => return Err(e.into()),
+        };
+    };
 
-
-    Ok(())
+    Ok(handlers)
 }
